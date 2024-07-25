d2b8b552692500e733a8de7e205c9b0a5fe43e58
semihbayindir <47758629+semihbayindir@users.noreply.github.com>
2024-07-25 16:15:51 +0300

    Project setup

---
+++ app/_layout.tsx
@@ -0,0 +1,17 @@
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+
+ export default function RootLayout() {
+
+  useFonts({
+    'openSans':require('./../assets/fonts/OpenSans-Light.ttf'),
+    'openSans-medium':require('./../assets/fonts/OpenSans-Medium.ttf'),
+    'openSans-bold':require('./../assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  return (
+      <Stack>
+        <Stack.Screen name='index' />
+      </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View style={{
+      flex:1,
+    }}>
+      <Text style={{fontFamily:'openSans-bold', fontSize:30}}>Hey!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
